086d9f78ee6083cca00be9b8d1c06f746d97872d
strong-ery <dstrongbusiness@gmail.com>
2026-06-01 02:13:05 -0600

    Initial commit

---
+++ src/client/java/typhonic/wavestone/client/WavestoneClient.java
@@ -0,0 +1,10 @@
+package typhonic.wavestone.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WavestoneClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
